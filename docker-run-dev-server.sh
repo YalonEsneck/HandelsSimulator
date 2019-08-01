d1293559ffa866058d79fee9e6e0ff81a774e5c5
Jan Merkelbag <yalon.esneck@gmail.com>
2019-08-01 22:32:16 +0200

    create custom Docker image to enable PHP extension gd

--- docker-run-dev-server.sh
+++ docker-run-dev-server.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 rootDir=$(realpath `dirname ${0}`)
 webDir=${rootDir}/WebInterface
+
+docker build --pull -t handelssimulator:dev docker/dev-server && \
+\
 docker run \
   --rm \
   --name handelsSimulator-dev \
   -v ${rootDir}/docker/dev-server/files/config/sites-enabled:/etc/apache2/sites-enabled \
   -v ${webDir}:/var/www/html \
   -p 4000:80 \
-  php:apache
+  handelssimulator:dev
